42962510dc044b0180fd1f4d2bececbe3f207249
Juan Martinez <jjmcarrascosa@gmail.com>
2024-06-08 20:40:30 +0100

    Chunk lazy loading working

--- LordOfTheRingsUnity/Assets/Packages/HoneyFramework/Scripts/World/HexTools/HexPathFinder.cs
+++ LordOfTheRingsUnity/Assets/Packages/HoneyFramework/Scripts/World/HexTools/HexPathFinder.cs
@@ -1,15 +1,3 @@
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace HoneyFramework
-{
-    /*
-     * Simple call to produce world graph. More functionality could be provided to cover or translate advantages of the A* into hex world
-     */
-    public class HexPathFinder
-    {
-        
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e453ea5dfd2491c020b4092bca31eb3bce4dcfc87af10f4eba3b00cb56e2a5ff
+size 346
